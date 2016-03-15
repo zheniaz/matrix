6b3f5f3b8f797ff00fcc72500702181c32d3d751
zheniaz <zhenia.zgurovets@gmail.com>
2016-03-16 00:39:08 +0200

    add, mult, trans. matrix

---
+++ Matrix/ConsoleApplication1/Matrix.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Matrix
+{
+    class Matrix
+    {
+        int[,] tm { get; set; }
+        int size1 { get; set; }
+        int size2 { get; set; }
+        public Matrix(int[,] arg, int i, int j)
+        {
+            tm = arg;
+            size1 = i;
+            size2 = j;
+        }
+
+
+        public void Addition(Matrix matrix2)
+        {
+            Console.WriteLine("сложить матрицы:");
+            int[,] res = new int[size1, size2];
+            for (int i = 0; i < this.size1; i++)
+            {
+                for (int j = 0; j < this.size2; j++)
+                {
+                    res[i, j] = this.tm[i, j] + matrix2.tm[i, j];
+                    Console.Write(res[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void multiplication(Matrix matrix2)
+        {
+            Console.WriteLine("умножить матрицы:");
+            int[,] res = new int[this.size1, matrix2.size2];
+            for (int i = 0; i < this.size1; i++)
+            {
+                for (int j = 0; j < matrix2.size2; j++)
+                {
+                    for (int k = 0; k < matrix2.size1; k++)
+                        res[i, j] += this.tm[i, k] * matrix2.tm[k, j];
+                    Console.Write(res[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void transposition()
+        {
+            Console.WriteLine("транспонировать матрицу:");
+            for (int i = 0; i < this.size1; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < this.size2; j++)
+                {
+                    Console.Write(this.tm[i, j] + "  ");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("после транспонирования:");
+            int[,] M_tr = new int[this.size2, this.size1];
+            for (int i = 0; i < this.size2; i++)
+            {
+                for (int j = 0; j < this.size1; j++)
+                {
+                    M_tr[i, j] = this.tm[j, i];
+                    Console.Write(M_tr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Show_Matrix()
+        {
+            Console.WriteLine("показать матрицу:");
+            for (int i = 0; i < this.size1; i++)
+            {
+                for (int j = 0; j < this.size2; j++)
+                {
+                    Console.Write(this.tm[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Matrix/ConsoleApplication1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите размерность матриц: ");
+                int n = int.Parse(Console.ReadLine());
+                int m = int.Parse(Console.ReadLine());
+                int[,] matrix1 = new int[n, m];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write("arr[ {0}, {1}] = ", i, j);
+                        matrix1[i, j] = int.Parse(Console.ReadLine());
+                    }
+                }
+                Console.WriteLine();
+
+                int[,] matrix2 = new int[n, m];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        Console.Write("arr[ {0}, {1}] = ", i, j);
+                        matrix2[i, j] = int.Parse(Console.ReadLine());
+                    }
+                }
+                Console.WriteLine();
+                Matrix m1 = new Matrix(matrix1, n, m);
+                Matrix m2 = new Matrix(matrix2, n, m);
+
+                int a;
+                do
+                {
+                    {
+                        Console.WriteLine(@"Hello, World! Please,  type the number:
+                    1.  matrix addition
+                    2.  multiplication of matrices
+                    3.  transposition of matrices
+                    4.  Show_Matrix 1
+                    5.  Show_Matrix 2
+                    ");
+
+                        try
+                        {
+                            a = int.Parse(Console.ReadLine());
+
+                            switch (a)
+                            {
+                                case 1:
+                                    if (matrix1.GetLength(0) == matrix2.GetLength(0) &&
+                                    matrix1.GetLength(1) == matrix2.GetLength(1))
+                                    {
+                                        m1.Addition(m2);
+                                    }
+                                    else
+                                    {
+                                        throw new Exception("Недопустимая для сложения размерность матриц.");
+                                    }
+                                    break;
+
+                                case 2:
+                                    if (matrix1.GetLength(0) == matrix2.GetLength(1) &&
+                                    matrix1.GetLength(1) == matrix2.GetLength(0))
+                                    {
+                                        m1.multiplication(m2);
+                                    }
+                                    else
+                                    {
+                                        throw new Exception("Недопустимая для умножения размерность матриц."); 
+                                    }
+                                    break;
+
+                                case 3:
+                                    m1.transposition();
+                                    break;
+
+                                case 4:
+                                    m1.Show_Matrix();
+                                    break;
+
+                                case 5:
+                                    m2.Show_Matrix();
+                                    break;
+
+                                default:
+                                    Console.WriteLine("Exit");
+                                    break;
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Error: " + e.Message);
+                        }
+                    }
+                    Console.WriteLine("Press Spacebar to exit; press any key to continue");
+                }
+                while (Console.ReadKey().Key != ConsoleKey.Spacebar);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
